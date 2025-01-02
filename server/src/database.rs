4b2cfb300bd706aed9ee54f78b9faffd5462d543
ryotarofr <ryoryo.fr0608@icloud.com>
2025-01-02 19:05:21 +0900

    del

--- server/src/database.rs
+++ server/src/database.rs
@@ -19,32 +19,3 @@ pub async fn connection() -> &'static Database {
         })
         .await
 }
-
-// use mongodb::{
-//     bson::doc,
-//     options::{ClientOptions, ServerApi, ServerApiVersion},
-//     Client,
-// };
-
-// #[tokio::main]
-// async fn main() -> mongodb::error::Result<()> {
-//     let mut client_options =
-//         ClientOptions::parse("mongodb+srv://ryotarofr:tarou.FR0608@mijinko.30pjp.mongodb.net/?retryWrites=true&w=majority&appName=mijinko")
-//             .await?;
-
-//     // Set the server_api field of the client_options object to set the version of the Stable API on the client
-//     let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
-//     client_options.server_api = Some(server_api);
-
-//     // Get a handle to the cluster
-//     let client = Client::with_options(client_options)?;
-
-//     // Ping the server to see if you can connect to the cluster
-//     client
-//         .database("admin")
-//         .run_command(doc! {"ping": 1})
-//         .await?;
-//     println!("Pinged your deployment. You successfully connected to MongoDB!");
-
-//     Ok(())
-// }
